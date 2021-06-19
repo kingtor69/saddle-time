288f1b53db6024b73ba67ecb89ebc9d1c8fd6b8d
= <tor@hearkitty.com>
2021-06-19 16:02:34 -0600

    working on import bug in api.py

--- api.py
+++ api.py
@@ -1,16 +1,14 @@
 import requests
-from secrets import ORS_API_KEY, MQ_API_KEY, OW_API_KEY
-
+from secret import ORS_API_KEY, MQ_API_KEY, OW_API_KEY
 
 ORS_API_BASE_URL = "https://api.openrouteservice.org/v2/directions/"
 MQ_API_BASE_URL = "http://www.mapquestapi.com/geocoding/v1/"
 OW_API_BASE_URL = "api.openweathermap.org/data/2.5/"
 
-# TODO: see "should" in docstring:
 def geocode_from_location(loc):
     """Returns lattitude and longitute for given location, generally an address. 
     Return False if none found. 
-    should Return list of choices if multiple matches found.
+    Returns a list of choices, could be one item long, could be many depending on the search parameters (e.g. searching for 'Albuquerque' yields 2 results: one in NM, USA and one in Brazil).
     """
     resp = requests.get(f'{MQ_API_BASE_URL}address', params={'key': MQ_API_KEY, 'location': loc})
     # return resp
