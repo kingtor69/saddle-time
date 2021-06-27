1ce24062908a8ba63fbd900b73e03df6bd2f70d0
= <tor@hearkitty.com>
2021-06-27 15:01:13 -0600

    not working, adding jasmine testing

--- README.md
+++ README.md
@@ -12,6 +12,14 @@ APIs to be used can be found in api.py
     - mountain
  - Open Weather: gives weather information (current and forecast) about route location
 
+### Python unittests were working before I changed the schema, but needs to be reworked
+
 ### Weather API is working with hard-coded location
+ - Still working on javascript to change location. 
+   - It partly* works for a location change, not at all for a temperature change.
+   - * by which I mean the data is arriving from the API, but not being added to the DOM correctly
+ - ...and the javascript to change units is unstarted yet, except a few document.querySelectors
 
 ### Home page looks good for no logged in user
+
+### adding Jasmine testing
--- static/weather.js
+++ static/weather.js
@@ -3,6 +3,15 @@
 const weatherCityInput = document.querySelector('#weather-city-input');
 const browserLocation = document.querySelector('#browser-location-select');
 const unitsSelector = document.querySelector('#units-selector');
+let units = unitsSelector.value;
+const unitsOptionMetric = document.querySelector('option.metric-option')
+const unitsOptionImperial = document.querySelector('option.imperial-option')
+const weatherConditionsHeader = document.querySelector('#weather-conditions-row')
+const weatherConditions = document.querySelector('#weather-conditions')
+const weatherIcon = document.querySelector('#weather-icon')
+const weatherDetails = document.querySelector('#weather-details')
+
+
 const baseApiUrl = "/api/";
 
 weatherCityInput.addEventListener('click', function() {
@@ -15,7 +24,7 @@ weatherCityInput.addEventListener('keypress', function(e) {
     if (e.key === 'Enter' || e.key === 'Return') {
         errorDiv.innerHTML = ""
         weatherCityInput.classList.add('city-is-set');
-        updateWeather(weatherCityInput.value, unitsSelector.value);
+        updateWeather(weatherCityInput.value, units);
     };
 });
 
@@ -37,14 +46,9 @@ async function updateWeather(location, units, geocode) {
     };
 };
 
-function updateWeatherDOM(weather) {
-    console.log(weather.city)    
-};
-
-//////////////////////////// this isn't working, but it's here:
+//TODO: this isn't working, but it's here:
 browserLocation.addEventListener('click', function() {
     const default_location = "Albuquerque, NM 87102 USA"
-    let theyAreHere;
     // let nudge = document.getElementById("nudge");
   
     // let showNudgeBanner = function() {
@@ -73,8 +77,58 @@ browserLocation.addEventListener('click', function() {
     };
 
     let geoError = function(error) {
-      theyAreHere = default_location
+        updateWeather(default_location, units, null)
     };
   
     navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
   });
+
+// TODO: add unitsSelector eventListener
+
+function updateWeatherDOM(weather) {
+    weatherConditionsHeader.innerHTML = ""
+    weatherDetails.innerHTML = ""
+    weatherCityInput.value = weather.city;
+    if (weather.units === "imperial") {
+        unitsOptionImperial.selected = 'selected';
+    } else {
+        unitsOptionMetric.selected = 'selected';
+    }
+    weatherConditions.innerText = weather.conditions;
+    weatherIcon.innerHTML = `<img src="{{ weather['weather_icon_url'] }}" />`
+    // this does it in alphabetical order... wtf?
+    let weatherDetailsObj = weather.current_weather_details;
+    console.log(weatherDetailsObj);
+    return
+    // for (let detail in weatherDetailsObj) {
+    //     const weatherTr = document.createElement('tr');
+    //     const categoryTd = document.createElement('td');
+    //     const conditionTd = document.createElement('td');
+    //     categoryTd.innerText = detail;
+    //     conditionTd.innerText = weather.current_weather_details[detail];
+    //     weatherTr.appendChild(categoryTd);
+    //     weatherTr.appendChild(conditionTd);
+    //     weatherDetails.appendChild(weatherTr);
+    // }
+    weatherDetailsKeys = keys(weatherDetailsObj);
+    for (let i=0; i < weatherDetailKeys.length; i++) {
+        console.log(`key ${i}: ${weatherDetailKeys[i]}`)        
+    //     const weatherTr = document.createElement('tr');
+    //     const categoryTd = document.createElement('td');
+    //     const conditionTd = document.createElement('td');
+    //     categoryTd.innerText = detail;
+    //     conditionTd.innerText = weather.current_weather_details[detail];
+    //     weatherTr.appendChild(categoryTd);
+    //     weatherTr.appendChild(conditionTd);
+    //     weatherDetails.appendChild(weatherTr);
+    }
+    // temp = weather.current_weather_details["Temperature"]
+    // feelsLike = weather.current_weather_details["Feels Like"]
+    // high = weather.current_weather_details["High"]
+    // low = weather.current_weather_details["Low"]
+    // humidity = weather.current_weather_details["Relative Humidity"]
+    // windSpeed = weather.current_weather_details["Wind Speed"]
+    // windDirection = weather.current_weather_details["Wind Direction"]
+
+};
+
