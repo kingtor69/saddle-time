8e66af5819563dcef4b582c3bf7bd4a65944dcf0
Tor Kingdon <tor@hearkitty.com>
2021-09-23 16:47:14 -0600

    cleaning still working

--- helpers.py
+++ helpers.py
@@ -187,31 +187,11 @@ def unit_markers(units):
 
 def login_session(user):
     """Log in a registered user to session."""
-
     session[CURR_USER] = user.id
 
 def logout_session():
     """Remove user who is logging out from session."""
-
     del session[CURR_USER]
-
-
-# def get_user_location_from_ip():
-#     """retrieve the user's location from their browser/IP address. Return a default location (currently Albuquerque, NM) if location can not be determined."""
-#     #!/usr/bin/env python 
-
-#     url = 'http://freegeoip.net/json/'
-#     try:
-#         with closing(urlopen(url)) as response:
-#             location = json.loads(response.read())
-#             print(location)
-#             location_city = location['city']
-#             location_state = location['region_name']
-#             location_country = location['country_name']
-#             location_zip = location['zipcode']
-#     except:
-#         return "Albuquerque NM USA 87102"
-
 
 def wind_direction_logical(degrees):
     """Give wind direction a logical name adapted from degrees.
@@ -226,8 +206,8 @@ def wind_direction_logical(degrees):
 
     index = int(biased_degrees / 45)
     indices = ["Northerly", "Northeasterly", "Easterly", "Southeasterly", "Southerly", "Southwesterly", "Westerly", "Northwesterly"]
+
     return indices[index]
-    
     
 def string_from_geocode(geocode):
     """creates an html-friendly string from geocode"""
@@ -260,22 +240,11 @@ def parse_geocode(arguments):
             999-lat: "42.3487",
             999-lng: "-83.0567"
         }
-    Currently, two services are supported:
-        *** ORS is for future development, so written code is commented out ***
-        # ORS (Open Route Service):
-        #     returns the array ORS expects for route parameters
-        #     i.e. [ [-85.5872,42.2917], [-83.0567,42.3487] ]
-        Mapbox:
-            returns string mapbox expects for the route parameters
-            i.e. '-85.5872,42.2917;-83.0567,42.3487'
-        NOTE: this method can parse and return data for both services, but is currently only doing so for mapbox (commented out references to geoarray are for ORS)
-        """
+    returns string mapbox expects for the route parameters
+    i.e. '-85.5872,42.2917;-83.0567,42.3487'
+    """
 
-    # these variables will be the return value
-    # geoarray = []
     geostring = ""
-
-    # termp variables used to sort everything properly
     id_list = []
     sortable_args = {}
     lat = False
@@ -298,18 +267,13 @@ def parse_geocode(arguments):
             except:
                 errors["garbage in garbage out error"] = f"{key} can't be parsed to an id"
 
-    # now build the array and string
     for i in sorted(id_list):
-        # geoarray.append([sortable_args[i]['lng'],sortable_args[i]['lat']])
         geostring+=f"{sortable_args[i]['lng']},{sortable_args[i]['lat']};"
-
-    # remove trailing semi-colon
     geostring = geostring[:-1]
     
     if errors:
         return {"errors": errors}
         
-    # return (geoarray, geostring)
     return geostring
 
 
