c345b971cb20fe6e8985e8635881dc52edd80d97
Tor Kingdon <tor@hearkitty.com>
2021-09-23 17:04:22 -0600

    weather.js done

--- static/js/weather.js
+++ static/js/weather.js
@@ -1,6 +1,5 @@
-// TODO: make browser location work
 console.log('weather.js');
-// mapLat and mapLng are generated in mapbox.js, which has already run when this does.
+// mapLat and mapLng are declared in mapbox.js, which has already run when this does.
 const unitsSelector = document.querySelector('#units-selector');
 const unitsOptionMetric = document.querySelector('option.metric-option');
 const unitsOptionImperial = document.querySelector('option.imperial-option');
@@ -24,8 +23,6 @@ if (queryString.location) {
         return resp.data.location;
     });
 };
-
-// let weatherLocation = (localStorage['weatherLocation']) ; defaultLocation;
 
 unitsSelector.addEventListener('change', function(evt) {
     units = evt.target.value;
@@ -69,21 +66,14 @@ function updateWeatherDOM(weather, lat, lng) {
         unitsOptionMetric.selected = 'selected';
         unitsOptionImperial.selected = '';
     } else {
-        // this is where it breaks (see TODO on line 196)
         throw new Error('invalid weather units');
     }
 
-    // update conditions headline
-    // TODO: this is not working when using select2 to change locations, that block disappears
-    // which might be because of the error being thrown above.... 
     weatherConditions.innerText = weather.conditions;
     weatherIcon.innerHTML = `<img src="${weather.weather_icon_url}">`
     weatherConditionsHeader.appendChild(weatherConditions)
     weatherConditionsHeader.appendChild(weatherIcon)
     
-    // TODO: city name is also not updating
-    $('#weather-city').text(weather.city)
-
     // gather DOM and data for details
     weatherDetailKeysTds = document.querySelectorAll('td.weather-detail-key')
     weatherDetailValueTds = document.querySelectorAll('td.weather-detail-value')
@@ -126,51 +116,5 @@ weatherLocationSelector.change((e) => {
     const [units, mapLat, mapLng] = processAutocomplete(e, weatherLocationSelector, 'weather');
     updateWeather(units, mapLat, mapLng);
     centerMap(mapLat, mapLng);
-    // console.log('remove bluePointer?');
-    // if (map.hasImage(`bluePointer`)) { map.removeImage(`bluePointer`) };
     placeMarker('blue', 'urhere', mapLat, mapLng);
 });
-
-// TODO: this is some semblance of logic for the browswer location
-// need to add class="mapbox-location-selector form-control" because flaskapparently it goes away when select2 is turned on
-// maybe because it's inside a Bootstrap "modal:"
-// https://select2.org/troubleshooting/common-problems
-
-// using browser location isn't working and currently deactivated
-// TODO: fix and activate it
-// browserLocationButton.addEventListener('click', function() {
-//     const default_location = "949 Montoya St NW, Albuquerque, NM 87104"
-//     // let nudge = document.getElementById("nudge");
-  
-//     // let showNudgeBanner = function() {
-//     //   nudge.style.display = "block";
-//     // };
-  
-//     // let hideNudgeBanner = function() {
-//     //   nudge.style.display = "none";
-//     // };
-  
-//     // let nudgeTimeoutId = setTimeout(showNudgeBanner, 5000);
-  
-//     let geoSuccess = function(position) {
-//         lat = position.coords.latitude;
-//         lng = position.coords.longitude;
-//         geocode = [lat, lng];
-//         updateWeather(null, units, (lat, lng))
-//         // hideNudgeBanner();
-//         // We have the location, don't display banner
-//         // clearTimeout(nudgeTimeoutId); 
-        
-        
-//         // Do magic with location
-//         // document.getElementById('startLat').innerHTML = position.coords.latitude;
-//         // document.getElementById('startLon').innerHTML = position.coords.longitude;
-//     };
-
-//     let geoError = function(error) {
-//         updateWeather(default_location, units, null)
-//     };
-  
-//     navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
-// });
-
