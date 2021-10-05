967be3bb4df5d71effc6a83497ce166e33548cdb
Tor Kingdon <tor@hearkitty.com>
2021-10-05 11:12:32 -0600

    REMOVED USER DELETE FUNCTIONALITY

--- static/js/users.js
+++ static/js/users.js
@@ -1,34 +1,5 @@
 console.log('users.js');
 const defaultLocationTd = document.querySelector('#default-location-autocomplete');
-const userDeleteButt = document.querySelector('#user-delete');
-const deleteConfirmation = document.querySelector('#delete-confirmation')
-const userDeleteForm = document.querySelector('#user-delete-form');
-const deleteCancelButt = document.querySelector('#delete-cancel');
-const deleteUnconfirmed = document.querySelector('#delete-unconfirmed');
-// defaultLocationSelector is declared in mapbox.js as a jQuery object;
-
-userDeleteButt.addEventListener('click', (e) => {
-    e.preventDefault();
-    deleteUnconfirmed.hidden = true;
-    deleteConfirmation.hidden = false;
-});
-
-deleteCancelButt.addEventListener('click', (e) => {
-    e.preventDefault();
-    deleteConfirmation.hidden = true;
-    deleteUnconfirmed.hidden = false;
-});
-
-userDeleteForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (deleteOrDont(e, 'users')) {
-        location.href='/logout?deleted=true';
-    } else {
-        const deleteWarning = document.querySelector('#delete-warning');
-        deleteWarning.innerText = "That was not correct. Please try entering your username again or cancel.";
-    };
-});
-
 selectTwo(defaultLocationSelector);
 defaultLocationSelector.change((evt) => {
     evt.preventDefault();
